88a3512acbb5b34bb0f7a51360a8a6bb8ffb6607
MachineSpiritJazzCafe <machinespiritjazzcafe@gmail.com>
2025-12-23 12:57:58 -0800

    DoubleLinkedList: Implementation p2 completed

--- src/main/scala/datastructures/DoublyLinkedList.scala
+++ src/main/scala/datastructures/DoublyLinkedList.scala
@@ -1,5 +1,7 @@
 package datastructures
 
+import java.lang.runtime.TemplateRuntime
+
 class DoublyLinkedList{
   var length: Int = 0
   var head: Option[BiNode] = None
@@ -12,12 +14,13 @@ class DoublyLinkedList{
       temp = temp.flatMap(_.next)
   }
 
-  def append(value: Int): Unit = {
+  def append(value: Int): Boolean = {
     val node = BiNode(value, None, tail)
     if (length < 1) head = Some(node)
     tail.foreach(_.next = Some(node))
     tail = Some(node)
     length += 1
+    true
   }
 
   def pop(): Option[BiNode] = {
@@ -29,6 +32,83 @@ class DoublyLinkedList{
     length -= 1
     if (length == 0) head = None 
     popped
+  }
+
+  def prepend(value: Int): Boolean = {
+    val node = BiNode(value, head, None)
+    head.foreach(_.prev = Some(node))
+    head = Some(node)
+
+    length += 1
+    if (length == 1) tail = Some(node)
+    true
+  }
+
+  def popFirst(): Option[BiNode] = {
+    val temp = head
+    head = temp.flatMap(_.next)
+    head.foreach(_.prev = None)
+    temp.foreach(_.next = None)
+    if (temp.isDefined) length -= 1
+    if (length == 0) tail = None
+    temp
+  }
+
+   def get(index: Int): Option[BiNode] = {
+    if (index < 0 || index >= length) return None
+
+    var temp: Option[BiNode] = None
+    
+    if (index < (length / 2))
+      temp = head
+      (0 until index).foreach { _ =>
+        temp = temp.flatMap(_.next)
+      }
+    else
+      temp = tail
+      ((length - 1) until index by -1).foreach { _ =>
+        temp = temp.flatMap(_.prev)
+      }
+    temp      
+  }
+
+
+  def set(index: Int, value: Int): Boolean = get(index) match {
+    case Some(node) => node.value = value; true
+    case None => false
+  }
+
+  def insert(index: Int, value: Int): Boolean = index match {
+    case 0 => prepend(value)
+    case i if (i == length) => append(value)
+    case i if (i < 0 || i > length) => false
+    case _ =>
+      var pre = get(index - 1)
+      var next = pre.flatMap(_.next)
+      val node = BiNode(value, next, pre)
+      pre.foreach(_.next = Some(node))
+      next.foreach(_.prev = Some(node))
+      length += 1
+      true
+  }
+
+  def remove(index: Int): Option[BiNode] = index match {
+    case 0 => popFirst()
+    case i if (i == length -1) => pop()
+    case _ =>
+      var temp = get(index)
+      var prev = temp.flatMap(_.prev)
+      var next = temp.flatMap(_.next)
+
+      prev.foreach(_.next = next)
+      next.foreach(_.prev = prev)
+
+      temp.foreach{ t =>
+        t.prev = None
+        t.next = None
+        length -= 1
+      }
+      temp
   }
 }
 
--- src/test/scala/datastructures/DoublyLinkedListSpec.scala
+++ src/test/scala/datastructures/DoublyLinkedListSpec.scala
@@ -54,4 +54,189 @@ class DoublyLinkedListSpec extends AnyFlatSpec with Matchers {
     list.head.get.next shouldBe list.tail
     list.tail.get.prev shouldBe list.head 
   }
+
+  "prepend" should "add single element to empty list" in {
+    val list = DoublyLinkedList.empty()
+
+    list.prepend(1)
+
+    list.head.get.value shouldBe 1
+    list.head.get.next shouldBe None
+    list.head.get.prev shouldBe None
+    list.tail.get.value shouldBe 1
+    list.tail.get.prev shouldBe None
+    list.tail.get.next shouldBe None
+    list.length shouldBe 1
+  }
+  
+  it should "add element to non empty list" in {
+    val list = DoublyLinkedList.fromArray(Array(1,2,3))
+
+    list.prepend(0)
+
+    list.head.get.value shouldBe 0
+    list.head.get.prev shouldBe None
+    list.head.get.next.get.value shouldBe 1
+    list.tail.get.value shouldBe 3
+  }
+
+  "popFirst" should "return None from empty list" in {
+    val list = DoublyLinkedList.empty()
+
+    val popped = list.popFirst()
+
+    popped shouldBe None
+    list.length shouldBe 0
+  }
+
+  it should "return element and empty the list from remainig element" in {
+    val list = DoublyLinkedList.fromArray(Array(1))
+
+    val popped = list.popFirst()
+
+    popped.get.value shouldBe 1
+    list.head shouldBe None
+    list.tail shouldBe None
+    list.length shouldBe 0
+  }
+
+  it should "return first element from non empty list" in {
+    val list = DoublyLinkedList.fromArray(Array(1,2,3,4))
+
+    val popped = list.popFirst()
+    
+    popped.get.value shouldBe 1
+    list.head.get.value shouldBe 2
+    list.head.get.next.get.value shouldBe 3
+    list.head.get.prev shouldBe None
+    list.tail.get.value shouldBe 4
+    list.length shouldBe 3
+  }
+
+  "get" should "return None from empty list" in {
+    val list = DoublyLinkedList.empty()
+
+    list.get(1) shouldBe None
+  }
+
+  it should "return node from 0 index" in {
+    val list = DoublyLinkedList.fromArray(Array(1))
+
+    list.get(0).get.value shouldBe 1
+    list.head.get.value shouldBe 1
+    list.tail.get.value shouldBe 1
+  }
+
+  it should "return node from last index" in {
+    val list = DoublyLinkedList.fromArray(Array(1,2,3))
+
+    list.get(2).get.value shouldBe 3
+  }
+
+  it should "return node from the middle" in {
+    val list = DoublyLinkedList.fromArray(Array(1,2,3,4,5))
+   
+    list.get(2).get.value shouldBe 3
+  }
+
+  "set" should "return false for empty list" in {
+    val list = DoublyLinkedList.empty()
+
+    list.set(0, 1) shouldBe false
+    list.length shouldBe 0
+  }
+
+  it should "return true and set value in head and tail" in {
+    val list = DoublyLinkedList.fromArray(Array(1,2,3))
+
+    list.set(0, 11)
+    list.head.get.value shouldBe 11
+
+    list.set(2, 33)
+    list.tail.get.value shouldBe 33
+    list.length shouldBe 3
+  }
+
+  it should "return true and set value in the middle" in {
+    val list = DoublyLinkedList.fromArray(Array(1,2,3,4,5))
+
+    list.set(2, 33) 
+    DoublyLinkedList.toArray(list).toSeq shouldBe Seq(1,2,33,4,5)
+  }
+
+  "insert" should "set a value under index 0 to an empty list" in {
+    val list = DoublyLinkedList.empty()
+
+    list.insert(0, 0)
+    
+    list.length shouldBe 1
+    list.head.get.value shouldBe 0
+    list.tail.get.value shouldBe 0
+  }
+
+  it should "return false if index is < 0 or > length" in {
+    val list = DoublyLinkedList.fromArray(Array(1,2,3))
+
+    list.insert(-1, 0) shouldBe false
+    list.insert(4, 0) shouldBe false
+    
+    DoublyLinkedList.toArray(list).toSeq shouldBe Seq(1,2,3)
+  }
+
+  it should "append value to a non empty list at the beginning" in {
+    val list = DoublyLinkedList.fromArray(Array(1,2,3))
+
+    list.insert(0, 0) shouldBe true 
+    DoublyLinkedList.toArray(list).toSeq shouldBe Seq(0,1,2,3)
+  }
+
+  it should "set a value to a non empty list at the end" in {
+    
+    val list = DoublyLinkedList.fromArray(Array(1,2,3))
+
+    list.insert(3, 4) shouldBe true 
+    DoublyLinkedList.toArray(list).toSeq shouldBe Seq(1,2,3,4)
+  }
+
+  it should "set a value in the middle" in {
+    
+    val list = DoublyLinkedList.fromArray(Array(1,2,3))
+
+    list.insert(2, 22) shouldBe true
+    
+    DoublyLinkedList.toArray(list).toSeq shouldBe Seq(1,2,22,3)
+  }
+
+  "remove" should "return None from empty list" in {
+    DoublyLinkedList.empty().remove(0) shouldBe None
+  }
+
+  it should "return head node and update list" in {
+    val list = DoublyLinkedList.fromArray(Array(1,2,3,4))
+    
+    list.remove(0).get.value shouldBe 1
+    list.length shouldBe 3
+    list.head.get.value shouldBe 2
+    DoublyLinkedList.toArray(list).toSeq shouldBe Seq(2,3,4)
+  }
+
+  it should "return tail node and update list" in {
+    val list = DoublyLinkedList.fromArray(Array(1,2,3,4))
+    
+    list.remove(3).get.value shouldBe 4
+    list.remove(0).get.value shouldBe 1
+    list.length shouldBe 2
+    list.tail.get.value shouldBe 3
+    list.head.get.value shouldBe 2
+    DoublyLinkedList.toArray(list).toSeq shouldBe Seq(2,3)
+  }
+  
+  it should "return middle node and update list" in {
+    
+    val list = DoublyLinkedList.fromArray(Array(1,2,3,4))
+    
+    list.remove(2).get.value shouldBe 3
+    list.length shouldBe 3
+    DoublyLinkedList.toArray(list).toSeq shouldBe Seq(1,2,4)
+  }
 }
