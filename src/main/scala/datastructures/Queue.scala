a9a2dd1b4a20829419a478d29cffc35891fcf9a8
MachineSpiritJazzCafe <machinespiritjazzcafe@gmail.com>
2025-12-24 10:47:00 -0800

    Queue implementation

--- src/main/scala/datastructures/Queue.scala
+++ src/main/scala/datastructures/Queue.scala
@@ -1,5 +1,56 @@
-package datastrcuctures
+package datastructures
 
-class Queue{}
+class Queue {
+  var lenght: Int = 0
+  var first: Option[Node] = None
+  var last: Option[Node] = None
 
-object Queue{}
+  def enqueue(value: Int) = {
+    val node: Option[Node] = Some(Node(value))
+    last.foreach(_.next = node)
+    if (first.isEmpty) first = node
+    last = node
+    lenght += 1
+  }
+
+  def dequeue(): Option[Node] = {
+    var temp = first.flatMap(_.next)
+    var deqNode = first
+    deqNode.foreach(_.next = None)
+    first = temp
+    deqNode.foreach(_ => lenght -= 1)
+    if (first.isEmpty) last = None
+    deqNode
+  }
+
+  def print(): Unit = {
+    var temp = first
+    while(temp.isDefined)
+      println(temp.get.value)
+      temp = temp.flatMap(_.next)  
+  }
+}
+
+object Queue {
+
+  def fromArray(arr: Array[Int]): Queue = {
+    val queue = new Queue()
+    arr.foreach(queue.enqueue(_))
+    queue
+  }
+
+  def toArray(queue: Queue): Array[Int] = {
+    var arr = Array.empty[Int]
+    var current = queue.first
+    if (queue.lenght < 0) arr
+    else
+      while (current.isDefined) {
+        arr = arr :+ current.get.value
+        current = current.get.next
+      }
+    arr  
+  }
+
+  def empty(): Queue = new Queue()
+}
+
---
+++ src/test/scala/datastructures/QueueSpec.scala
@@ -0,0 +1,65 @@
+package datastructures
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import java.nio.channels.NonWritableChannelException
+
+class QueueSpec extends AnyFlatSpec with Matchers {
+  
+  "Queue" should "create empty queue" in {
+    val queue = Queue.empty()
+    queue.first shouldBe None
+    queue.last shouldBe None
+    queue.lenght shouldBe 0
+  }
+  
+  it should "enqueue onto the queue" in {
+    val queue = Queue.empty()
+    queue.enqueue(1)
+    queue.first.get.value shouldBe 1
+    queue.last.get.value shouldBe 1
+    queue.lenght shouldBe 1
+  }
+
+  it should "enqueue onto non empty queue" in {
+    val queue = Queue.fromArray(Array(1,2))
+    queue.enqueue(3)
+    Queue.toArray(queue).toSeq shouldBe Seq(1,2,3)
+    queue.first.get.value shouldBe 1
+    queue.last.get.value shouldBe 3
+    queue.lenght shouldBe 3
+  } 
+  
+  it should "dequeue from the first" in {
+    val queue = Queue.fromArray(Array(1))
+
+    queue.dequeue().get.value shouldBe 1
+    queue.lenght shouldBe 0
+    queue.first shouldBe None
+  }
+
+  it should "enqueue and dequeue following LIFO" in {
+   val queue = Queue.empty()
+
+   queue.enqueue(1)
+   queue.first.get.value shouldBe 1
+   queue.last.get.value shouldBe 1
+   queue.enqueue(2)
+   queue.first.get.value shouldBe 1
+   queue.last.get.value shouldBe 2
+   queue.enqueue(3)
+   queue.first.get.value shouldBe 1
+   queue.last.get.value shouldBe 3
+   Queue.toArray(queue).toSeq shouldBe Seq(1,2,3)
+   queue.dequeue()
+   Queue.toArray(queue).toSeq shouldBe Seq(2,3)
+   queue.first.get.value shouldBe 2
+   queue.dequeue()
+   Queue.toArray(queue).toSeq shouldBe Seq(3)
+   queue.first.get.value shouldBe 3
+   queue.dequeue()
+   Queue.toArray(queue).toSeq shouldBe Seq()
+   queue.first shouldBe None
+   queue.last shouldBe None 
+  }
+}
